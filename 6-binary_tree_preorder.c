92926b1bb9eec86ca17854cb44f998d0f2675f8b
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-26 14:18:37 +0300

    added algoritn comment

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -20,6 +20,14 @@ if (func == NULL)
 return;
 
 /* the func parameter int binary_tree_s::n */
+/*
+Algorithm Preorder(tree)
+
+Visit the root.
+Traverse the left subtree, i.e., call Preorder(left->subtree)
+Traverse the right subtree, i.e., call Preorder(right->subtree) 
+*/
+
 func(tree->n);
 binary_tree_preorder(tree->left, func);
 binary_tree_preorder(tree->right, func);
