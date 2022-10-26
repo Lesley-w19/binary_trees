5518f62dc57bfa3249494ed6f390225a5eb1c9d7
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-26 13:31:02 +0300

    removed white triling space

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,7 +5,7 @@
  * @parent: is a pointer to the parent node of the node to create
  * @value: is the value to put in the new node
  *
- * Return: a pointer to the new node, or NULL on failure 
+ * Return: a pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
