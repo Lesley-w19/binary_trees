475e05130dcb535d8f6841b8e73439c583b2f449
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-27 17:15:11 +0300

    reduced number of characters in line 3

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - a function that measures the depth of a node in a binary tree
+ * binary_tree_height - a function that measures the depth of a
+ * node in a binary tree
  *
  * @tree: is a pointer to the root node of the tree to measure the height.
  *
