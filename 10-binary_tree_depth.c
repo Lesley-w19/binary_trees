84f7944f6928174d1d821922078ec2b0aa542a9e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-27 17:16:23 +0300

    added binary_tree_depth description

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - a function that measures the depth of a
+ * binary_tree_depth - a function that measures the depth of a
  * node in a binary tree
  *
  * @tree: is a pointer to the root node of the tree to measure the height.
