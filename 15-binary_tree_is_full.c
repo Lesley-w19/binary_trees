7781c7bab4ec43c6d8149f095b7f3651820daac7
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-27 18:34:09 +0300

    Added the NULL tree-> to return

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -13,6 +13,9 @@ int left_full, right_full;
 if (tree == NULL)
 return (0);
 
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+
 left_full = binary_tree_is_full(tree->left);
 right_full = binary_tree_is_full(tree->right);
 
