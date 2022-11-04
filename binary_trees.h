5c4c0c3934640c8a957b78a03cdf89084ce84cdf
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-11-04 15:17:08 +0300

    rearrange the binary_print

--- binary_trees.h
+++ binary_trees.h
@@ -27,7 +27,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -47,6 +47,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-void binary_tree_print(const binary_tree_t *);
 
 #endif 
